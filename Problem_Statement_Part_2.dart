f7c0b34a88dfb61276f41cb9c2cd7e4edde38a16
Susruth <UP945169@myport.ac.uk>
2020-12-20 23:36:50 +0000

    Added more files

--- Problem_Statement_Part_2.dart
+++ Problem_Statement_Part_2.dart
@@ -1,5 +1,11 @@
 import 'dart:io';
 
 void main(){
-  
+  print("Please enter your name: ");
+  String name = stdin.readLineSync();
+  print("Your name is $name");
+
+  print("Please enter your age: ");
+  int age = int.parse(stdin.readLineSync());
+  print("Your name is $name and you are $age old");
 }
