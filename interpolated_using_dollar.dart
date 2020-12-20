c131baf1c01d067778251b81299a5a83ee58c214
Susruth <UP945169@myport.ac.uk>
2020-12-20 23:30:33 +0000

    Added more stuff

---
+++ interpolated_using_dollar.dart
@@ -0,0 +1,5 @@
+void main(){
+  String country = 'United Kingdom';
+
+  print('I want to visit $country');
+}
---
+++ string_interpolation.dart
@@ -0,0 +1,6 @@
+void main(){
+  String s1 = 'first half';
+  String s2 = ' second half';
+
+  print(s1 + s2);
+}
