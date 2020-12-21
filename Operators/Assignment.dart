3aab573424aed22da9d047d9336871428c46b30d
Susruth <UP945169@myport.ac.uk>
2020-12-21 15:52:29 +0000

    Added more files

---
+++ Operators/Assignment.dart
@@ -0,0 +1,24 @@
+void main() {
+  var A = 10;
+  var B = 7;
+
+  print("Before compound assignment operator: ");
+  print(A);
+
+  A += B;
+
+  print("After using compound assignment: ");
+  print(A);
+
+  //&=
+  A = 10;
+  B = 3;
+
+  print("Before compound assignment operators: ");
+  print(A);
+
+  A &= B;
+
+  print("After using compound assignment operator: ");
+  print(A);
+}
