4b63c54036856217cbaffc6c40ed4c94f384667b
Susruth <UP945169@myport.ac.uk>
2020-12-22 10:55:44 +0000

    Git added

---
+++ Operators/Logical_Operators.dart
@@ -0,0 +1,7 @@
+void main() {
+  var A = true;
+  var B = false;
+  var expr = A && B;
+
+  print(expr);
+}
--- Operators/typeTest.dart
+++ Operators/typeTest.dart
@@ -1,7 +1,7 @@
 void main() {
   double type1 = 5.0;
   int type2 = 87;
-  String type3 = "Reductive";
+  // String type3 = "Reductive";
   bool type = true;
 
   print(type1 is int);
