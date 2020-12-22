ddc870a9254dd4a057063812f09ea709337df04e
Susruth <UP945169@myport.ac.uk>
2020-12-22 12:27:13 +0000

    Added bitwise.dart

---
+++ Operators/bitwise.dart
@@ -0,0 +1,19 @@
+void main() {
+  //bitwise
+  /* 
+  Bitwise AND - &
+  Bitwise OR - `
+  Bitwisw XOR - ^
+  the bitwise operators will convert the number to
+  binary and then binary numbers will be compared
+  if they fit the contraints then 
+  */
+
+  //Shift Operators
+  /* 
+  << Shift Left 
+  Shifts all bits of its operand to the left by the specified amount
+  >> Shift Right
+  Shifts all bits of its operand to the right by the specified amount
+  */
+}
