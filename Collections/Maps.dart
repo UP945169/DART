9b677496cb28d7630cbcc3da31032e605b1f4bc2
Susruth <UP945169@myport.ac.uk>
2020-12-23 21:59:46 +0000

    more added

---
+++ Collections/Maps.dart
@@ -0,0 +1,14 @@
+void main() {
+  var MadeaList = ['carrot', 'banana', 'cucumber'];
+
+  //we make a map with a name called vegtables thus allows
+  //us to print off all of the values in the map with
+  // 'I love each element'
+
+  var MapofList = MadeaList.map((vegtables) => 'I love $vegtables');
+
+  print(MapofList);
+
+  //the toList() method makes a map back to a list
+  MapofList.toList();
+}
