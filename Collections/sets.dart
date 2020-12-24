7787d848ae7ecb37934ba0752121982ed90483c0
Susruth Puppala <up945169@myport.ac.uk>
2020-12-24 17:31:50 +0000

    Added more stuff

---
+++ Collections/sets.dart
@@ -0,0 +1,22 @@
+void main() {
+  //literal way
+
+  var simpleset = {1, 2, 3};
+
+  print(simpleset);
+
+  //literal way with datatype
+
+  var setWithDataSet = <num>{1, 4, 6, 1.5, 7};
+
+  print(setWithDataSet);
+
+  //creating an empty set using a constructor
+
+  var emptySet = <int>{};
+
+  print(emptySet);
+
+  Set<int> anemptyset = {};
+  print(anemptyset);
+}
---
+++ Collections/workingWithSets.dart
@@ -0,0 +1,43 @@
+void main() {
+  //adding a single item to a set
+
+  Set<String> setOfFruit = {};
+
+  setOfFruit.add("apples");
+  setOfFruit.add("orange");
+  setOfFruit.add("banana");
+
+  print(setOfFruit);
+
+  //adding multiple items to a set
+
+  Set<String> newSet = {};
+
+  var moreFruits = {"Watermelon", "grapes"};
+  newSet.addAll(moreFruits);
+
+  print(newSet);
+
+  //finding length
+  print(newSet.length);
+
+  //removing items
+
+  moreFruits.remove('Watermelon');
+
+  print(moreFruits);
+
+  //check a set
+  print(setOfFruit.contains("apples"));
+
+  //intersection between two sets
+
+  var set1 = {"apples", "banana", "orange"};
+  var set2 = {"apples", "jackfruit", "watermelon"};
+
+  var intersection = set1.intersection(set2);
+
+  print(intersection);
+
+  //union between sets
+}
