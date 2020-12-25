9381379e3cba6bb7133d491931d8c2a9d42dece3
Susruth Puppala <up945169@myport.ac.uk>
2020-12-25 21:09:43 +0000

    Added more stuff

--- Control_Flow/break_continue.dart
+++ Control_Flow/break_continue.dart
@@ -1,1 +1,22 @@
-void main() {}
+void main() {
+  var intList = [7, 3, 5, 1, 5, 2, 5, 6];
+
+  for (var i in intList) {
+    if (i % 2 == 0) {
+      print(i);
+      // when the first even number is found the program stops
+      break;
+    }
+  }
+
+  //continue
+  var experience = [5, 1, 6, 8, 1, 2, 3, 4];
+
+  for (var i = 0; i < experience.length; i++) {
+    var candidateExperience = experience[i];
+    if (candidateExperience < 5) {
+      continue;
+    }
+    print("Call candidate $i for an interview");
+  }
+}
