868fb96a15653b78ede490f210bee614292037f5
Susruth Puppala <up945169@myport.ac.uk>
2020-12-25 19:25:46 +0000

    Added more files

--- Control_Flow/for_Loop.dart
+++ Control_Flow/for_Loop.dart
@@ -2,4 +2,26 @@ void main() {
   for (int i = 0; i < 5; i++) {
     print(i);
   }
+
+  //iterating through a collection of data
+
+  var collection = ["Blue", "Yellow", "Green", "Orange"];
+  for (int i = 0; i < collection.length; i++) {
+    print(collection[i]);
+  }
+
+  //for in loop
+
+  for (var i in collection) {
+    print(i);
+  }
+
+  //conditions with loops
+  var intList = [6, 7, 8, 9, 10, 11, 13];
+
+  for (var i in intList) {
+    if (i % 2 == 0) {
+      print(i);
+    }
+  }
 }
