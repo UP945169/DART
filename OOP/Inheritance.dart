79ccc56bf8ce92b1e95affb972637d14be935a8b
Susruth <UP945169@myport.ac.uk>
2021-01-04 23:19:41 +0000

    Added new files

--- OOP/Inheritance.dart
+++ OOP/Inheritance.dart
@@ -6,6 +6,24 @@ class Animal {
 
 class Cat extends Animal {
   String makeNoise() {
-    print("Purrr");
+    print("purr");
   }
+}
+
+class Pig extends Animal {
+  String makeNoise() {
+    print("oink");
+  }
+}
+
+main() {
+  Cat cat = new Cat();
+  cat.name = "Nora";
+  cat.legCount = 4;
+  cat.makeNoise();
+
+  Pig pig = new Pig();
+  pig.name = "Babe";
+  pig.legCount = 4;
+  pig.makeNoise();
 }
